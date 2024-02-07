a3c0be1fcaaeeee6bc318712910efbba258dd4d1
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2024-02-07 18:02:47 +0530

    🚚 setup client page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Project from "./pages/Project";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
